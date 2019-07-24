087bee5e5b0ac5569d8994efeaf67fcb8c175d3b
akio <medue8@gmail.com>
2019-07-24 15:14:46 +0700

    init

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Authon: akio <medue8@gmail.com>
+ * Date: 7/24/19
+ * Time: 15:03
+ */
+
+namespace App\Controller;
+
+
+class DefaultController
+{
+
+    public function index()
+    {
+        echo __CLASS__.':'.__FUNCTION__;exit;
+    }
+
+}
